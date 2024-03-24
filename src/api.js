98396449369a7a01d27bff23abaefd0a0a3d8f54
eteen <eteensmash@gmail.com>
2024-03-24 16:07:45 -0700

    start of js

--- src/api.js
+++ src/api.js
@@ -1,2 +1,15 @@
-const apiKey = 'ace96045fa13e8edd3816230134b5da4';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=milan'
+const apiFunc = () => {
+  const apiKey = 'ace96045fa13e8edd3816230134b5da4';
+  const apiUrl =
+    'https://api.openweathermap.org/data/2.5/weather?units=metric&q=milan';
+
+  async function checkWeather() {
+    const response = await fetch(`${apiUrl}&appid=${apiKey}`);
+    const data = await response.json();
+
+    console.log(data);
+  }
+  checkWeather();
+};
+
+export default apiFunc;
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import apiFunc from './api';
+
+apiFunc();
