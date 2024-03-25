5854f593d68da60aaa280c59bd3440e63afdcef2
eteen <eteensmash@gmail.com>
2024-03-24 20:42:50 -0700

    temp added and rendered to the screen

--- src/dataControl.js
+++ src/dataControl.js
@@ -1,6 +1,8 @@
 const dataControlFunc = async (data) => {
   const city = data.location.name;
   document.querySelector('.city').innerHTML = city;
-  console.log(city);
+
+  const temp = data.current.temp_c;
+  document.querySelector('.temp').innerHTML = `${temp}°C`;
 };
 export default dataControlFunc;
