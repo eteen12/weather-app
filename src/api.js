4854ad0a0a82e251d94fafcda4b3950ab7182514
eteen <eteensmash@gmail.com>
2024-03-25 20:39:51 -0700

    attempt at fixing broken link

--- src/api.js
+++ src/api.js
@@ -2,10 +2,10 @@ import dataControlFunc from './dataControl';
 import conditionsFunc from './conditions';
 
 const apiKey = '42e9a66ab9ea44fa9b133750242403';
-const apiUrl = 'https://api.weatherapi.com/v1/current.json?key=';
+const apiUrl = '://api.weatherapi.com/v1/current.json?key=';
 
 const apiFunc = async (city) => {
-  const response = await fetch(`${apiUrl + apiKey}&q=${city}&aqi=no`);
+  const response = await fetch(`https${apiUrl + apiKey}&q=${city}&aqi=no`);
   const data = await response.json();
   dataControlFunc(data);
   conditionsFunc(data, city);
